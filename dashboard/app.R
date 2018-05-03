9beded1c59a974e088bf5bca32bc2fa563ee846d
ybkamaleri <ybkamaleri@gmail.com>
2018-05-03 21:01:25 +0200

    conditionalPanel on sidemenu ulykke type

--- dashboard/app.R
+++ dashboard/app.R
@@ -11,22 +11,33 @@ source('data.R', local = TRUE)
 ############################# Sidebar ###############################
 sidebar <- dashboardSidebar(
   width = 300,
-  sidebarMenu(
-    menuItem("Helse enheten", tabName = "unit", icon = icon("building"),
-             selectInput(inputId = "unit", label = "Valg enhet",
-                         choices = c("Sykehus" = 1,
-                                     "HF" = 2,
-                                     "RHF" = 3),
-                         selected = 3,
-                         width = '98%')),
-    menuItem("Periode", tabName = "tid", icon = icon("calendar"),
-             dateRangeInput(inputId = "date", label = "Valg dato fra og til"),
-             start = Sys.Date() - 30, end = Sys.Date(),
-             separator = "-", format = "dd-mm-yyyy", startview = "year",
-             language = "no", weekstart = 1),
-    menuItem("Ulykke og Skadegradering", tabName = "skade", icon = icon("medkit")),
-    menuItem("Sykehusopphod", tabName = "dag", icon = icon("bed"))
-  ))
+  sidebarMenu(id = "sidebar",
+              menuItem("Helse enheten", tabName = "unit", icon = icon("building"),
+                       selectInput(inputId = "unit", label = "Valg enhet",
+                                   choices = c("Sykehus" = 1,
+                                               "HF" = 2,
+                                               "RHF" = 3),
+                                   selected = 3,
+                                   width = '98%')),
+              menuItem("Periode", tabName = "tid", icon = icon("calendar"),
+                       dateRangeInput(inputId = "date", label = "Valg dato fra og til"),
+                       start = Sys.Date() - 30, end = Sys.Date(),
+                       separator = "-", format = "dd-mm-yyyy", startview = "year",
+                       language = "no", weekstart = 1),
+              menuItem("Type ulykke", tabName = "skade", icon = icon("medkit")),
+              conditionalPanel("input.sidebar === 'skade'",
+                               selectInput("ulykke1", "Valg type ulykke:",
+                                           choices = list("Transportulykke" = 1,
+                                                          "Fallulykke" = 2,
+                                                          "Voldsulykke" = 3,
+                                                          "Arbeidsulykke" = 4,
+                                                          "Sport og fritid" = 5,
+                                                          "Brann og inhalasjonsskade" = 6,
+                                                          "Annen ulykke" = 7),
+                                           selected = 7,
+                                           width = '90%')),
+              menuItem("Sykehusopphod", tabName = "dag", icon = icon("bed"))
+              ))
 
 
 ############################## Body ##################################
