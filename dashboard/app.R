c0bcbda59b000b5219b8930a4a543681824525d4
Yusman Kamaleri <ybkamaleri@gmail.com>
2018-05-04 13:11:24 +0200

    skadeData fra skade og ulykke skjema

--- dashboard/app.R
+++ dashboard/app.R
@@ -26,7 +26,7 @@ sidebar <- dashboardSidebar(
                        language = "no", weekstart = 1),
               menuItem("Ulykke og skadegradering", tabName = "skade", icon = icon("medkit")),
               conditionalPanel("input.sidebar === 'skade'",
-                               selectInput("ulykke1", "Valg type ulykke:",
+                               selectInput(inputId = "ulykkeType", "Valg type ulykke:",
                                            choices = list("Transportulykke" = 1,
                                                           "Fallulykke" = 2,
                                                           "Voldsulykke" = 3,
@@ -50,7 +50,7 @@ body <-  dashboardBody(
                 box(
                   width = 4,
                   ## title = "Kroppsregion",
-                  selectInput("kropp", "Valg kroppsregion:",
+                  selectInput(inputId = "kropp", "Valg kroppsregion:",
                               choices = list("Head" = 1,
                                              "Face" = 2,
                                              "Neck" = 3,
@@ -64,7 +64,7 @@ body <-  dashboardBody(
                 box(
                   width = 4,
                   ## title = "Skadegradering",
-                  checkboxGroupInput("skade", "Valg skadegrader:",
+                  checkboxGroupInput(inputId = "sgrad", "Valg skadegrader:",
                                      choices = list("1" = 1,
                                                     "2" = 2,
                                                     "3" = 3,
@@ -106,10 +106,10 @@ server <- function(input, output, session) {
 
   ## Viser transport type hvis transportulykke
   output[["box"]] <- renderUI({
-    if (input$ulykke1 == 1)
+    if (input$ulykkeType == 1)
       box(
         width = 4,
-        selectInput("acc", "Transportulykke",
+        selectInput(inputId = "acc", "Transportulykke",
                     choices = list("Bil" = 1,
                                    "MC" = 2,
                                    "Sykkel" = 3,
@@ -122,8 +122,65 @@ server <- function(input, output, session) {
                     selected = 1))
   })
 
+  skadeData <- eventReactive(input$ulykkeType %in% 1:7, {
+
+    ## Valgte ais-koder
+    indAis <- grep("Valgte ais", names(skade)) #finne indeks til kolonne
+    names(skade)[indAis] <- "ais"  #gir nytt navn til Valgte ais-koder
+
+    ################################################
+    ## kombinere alle skadekoder fra samme NTR-nr
+    ## og tar bort dublikate koder
+    ###############################################
+    #ta bort alle missing NTR-nr.
+    skade <- skade[!is.na(ntrid), ]
+
+    ## kombinere alle skadekoder og valgt bare unike koder
+    ## fra forskjellige sykehus for hver NTR-nr og variable navn blir "aiskode"
+    skade[skade[!is.na(ntrid),
+                toString(unique(unlist(
+                  strsplit(ais, split = ",")))), by = ntrid],
+          on = "ntrid", aiskode := i.V1]
+
+
+    ### Beholder alle var i skadeskjema
+    ### alle var starter med i. kommer fra skade skjema
+    skadeUlykke <- ulykke[skade, on = "ntrid"]
+
+    ## henter index fra acc_trans til acc_fire
+    accName <- grep("acc_transport", names(skadeUlykke)):grep("acc_fire_inhal", names(skadeUlykke))
+    ## convert to numeric
+    for (d in accName) {
+      set(skadeUlykke, j = d, value = as.numeric(skadeUlykke[[d]]))
+    }
+
+    #########################
+    ## legger til HF og RHF
+    #########################
+    skadeUlykke[, i.UnitId := as.numeric(i.UnitId)]
+    resh[, reshid := as.numeric(reshid)]
+
+    resh[skadeUlykke, on = c(reshid = "i.UnitId")]
+
+  })
+
+
+  filSkadeData <- reactive({
+
+    ## merged data skade og ulykke
+    data <- skadeData()
+
+    ### Filter element##
+    helseEnhet <- input$unit
+
+    datoFra <- format(input$date[1])
+    datoTil <- format(input$date[2])
+
+    ulType <- input$ulykkeType
+  })
+
   output$table <- renderDataTable({
-    head(masterID)
+    skadeData()[1:10, 1:5]
   })
 
   output$skadegrad <- renderText({
