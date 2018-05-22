64f9b12ef0cf35496592cbd82c5313f81670c840
ybkamaleri <ybkamaleri@gmail.com>
2018-05-22 05:10:53 +0200

    check required packages else install if not found

--- ntrApp/global.R
+++ ntrApp/global.R
@@ -1,8 +1,17 @@
-
 library(shiny)
-library(shinydashboard)
-library(DT)
-library(data.table)
+
+#create a function to check for installed packages and install them if they are not installed
+install.packages <- function(packages){
+  new.packages <- packages[!(packages %in% installed.packages()[, "Package"])]
+  if (length(new.packages))
+    install.packages(new.packages, dependencies = TRUE)
+  sapply(packages, require, character.only = TRUE)
+}
+
+# usage
+req.packages <- c("shinydashboard", "DT", "data.table")
+install.packages(req.packages)
+
 
 ## useMe <- c("shiny", "shinydashboard", "DT", "data.table")
 ## lapply(useMe, require)
