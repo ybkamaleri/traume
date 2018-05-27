343ae1ffe7b13b56fa654f5316bf0bf7b9e70c81
ybkamaleri <ybkamaleri@gmail.com>
2018-05-28 00:30:10 +0200

    exclude -1 prob

--- doc/forslag.R
+++ doc/forslag.R
@@ -2,9 +2,19 @@
 #######################################
 ## Antall hver aldersgruppe per kjønn
 #######################################
+traume[PatientAge == -1, PatientAge := NA] #bytt -1 til NA
+## finner ut hvordan velger man -1 f.eks PatientAge!=-1 ikke funker. Koden nedenfor eksluderer ikke -1
+## demo <- traume[PatientAge > -1 | !is.na(PatientAge) , .N, keyby = list(PatientGender, PatientAge)]
 
-demo <- traume[!is.na(PatientAge) | PatientAge != -1, .N, by = list(PatientAge, PatientGender)]
+demo <- traume[!is.na(PatientAge) , .N, keyby = list(PatientGender, PatientAge)]
+
+## Summen for hver alderskategori
+demo[, Sum := sum(N), by = list(PatientAge)]
 
 library(ggplot2)
-ggplot(demo, aes(as.factor(PatientAge), N, group = PatientGender)) +
-  geom_line(aes(color = as.factor(PatientGender)))
+ggplot(demo, aes(as.factor(PatientAge), N, group = PatientGender, color = as.factor(PatientGender))) +
+  geom_line() + theme_linedraw()
+
+ggplot(demo) +
+  geom_line(aes(as.factor(PatientAge), N, group = PatientGender, color = as.factor(PatientGender))) +
+  geom_line(aes(as.factor(PatientAge), Sum))
