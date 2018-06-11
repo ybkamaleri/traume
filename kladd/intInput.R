71f6c41cc8c8587d82586f96264cec8de6ba7c20
Yusman Kamaleri <ybkamaleri@gmail.com>
2018-06-11 17:16:05 +0200

    now working dynamic input

--- kladd/intInput.R
+++ kladd/intInput.R
@@ -7,16 +7,29 @@ set.seed(1234)
 
 DT <- data.table(
   v1 <- c("Nord", "Midt", "Vest", "Sør", "Sør", "Nord", "Nord", "Nord"),
-  v2 <- sample(2:4, 8, replace = TRUE)
+  v2 <- rep(c("Satu", "Dua"), 8),
+  v3 <- sample(2:4, 8, replace = TRUE)
 )
 
 
 server <- function(input, output) {
 
-  output$sykehus <- renderUI({
-    selectInput("nama", "List sykehus", as.list(unique(DT$V1)))
+  namasyk <- eventReactive(input$nom, {
+    if (input$nom = 1){
+      namasyk <- unique(DT$V1)
+    } else {
+      namesyk <- unique(DT$V2)
+    }
   })
 
+  output[["syke2"]] <- renderUI({
+
+    nama <- as.list(namasyk())
+    selectInput("sykeIn", "Valg Sykehus", choices = nama)
+
+  })
+
+  output$syk3 <- renderText(input$sykeIn)
 }
 
 
@@ -24,13 +37,13 @@ ui <- fluidPage(
   titlePanel("Test dynamisk input"),
 
   sidebarPanel(
-    uiOutput("nama")
+    numericInput("nom", "Pilih nombor", value = 1:3),
+    uiOutput("syke2")
 
   ),
 
   mainPanel(
-
-    verbatimTextOutput("sykehus")
+    textOutput("syk3")
   )
 )
 
