6f681bf24f50d8a9f7a0bc649f134d166b1ef465
Yusman BK <ybkamaleri@gmail.com>
2018-08-25 11:33:48 +0200

    delete old test

--- ntrApp/debug.R
+++ ntrApp/debug.R
@@ -1,4 +1,19 @@
 ## Brukers for å kjøre debuging fordi C-c s funker ikke fro debuging
 
 library(shiny)
-runApp(getwd())
+appDir <- getwd()
+runApp(appDir)
+
+
+## Testing
+
+## Hvis ikke allerede installert
+library(devtools)
+install_github("rstudio/shinytest")
+
+## snapshot
+library(shinytest)
+recordTest(appDir)
+
+## test
+testApp(appDir, "mytest")
