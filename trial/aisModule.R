4cf7b8f32274a53635ba837b1b3ceac0363ecbec
Yusman BK <ybkamaleri@gmail.com>
2018-08-29 17:16:59 +0200

    UI for kroppsregion og skadegradering

--- trial/aisModule.R
+++ trial/aisModule.R
@@ -2,6 +2,7 @@
 ##################
 
 library(shiny)
+library(shinydashboard)
 library(data.table)
 library(ggplot2)
 
@@ -13,6 +14,7 @@ aisModUI <- function(id){
   fluidPage(
     fluidRow(
       box(width = 3,
+          title = "Type ulykke",
           selectInput(inputId = ns("ulykke"),
                       label = NULL,
                       choices = list("Transport" = 1,
@@ -22,21 +24,61 @@ aisModUI <- function(id){
                                      "Arbeid" = 5,
                                      "Sport og fritid" = 6,
                                      "Brann og inhalasjon" = 7,
-                                     "Annen" = 8),
+                                     "Annen" = 8,
+                                     "Alle type ulykker" = 9),
+                      selected = 9,
+                      width = '90%')),
+      box(width = 3,
+          title = "Kroppsregion",
+          selectInput(inputId = ns("kropp"),
+                      label = NULL,
+                      choices = list("Head" = 1,
+                                     "Face" = 2,
+                                     "Neck" = 3,
+                                     "Thorax" = 4,
+                                     "Abdomen" = 5,
+                                     "Upper extremity" = 7,
+                                     "Lower extremity" = 8,
+                                     "External and other" = 9),
                       selected = 1,
-                      width = '90%'))))
+                      multiple = TRUE,
+                      width = '99%')),
+      box(width = 3,
+          title = 'Skadegradering untatt grad 1',
+          checkboxGroupInput(inputId = "sgrad",
+                             label = NULL,
+                             choices = list("2" = 2,
+                                            "3" = 3,
+                                            "4" = 4,
+                                            "5" = 5,
+                                            "6" = 6),
+                             inline = TRUE,
+                             selected = 2
+                             ),
+          checkboxInput(inputId = ns("grad1"),
+                        label = " include skadegrad 1 i analysen"))
+    ))
 }
 
 aisMod <- function(input, output, session, data){
 
+
+
 }
 
 
 
-
-ui <- fluidPage(
-  aisModUI("ais")
+ui <- dashboardPage(
+  dashboardHeader(),
+  dashboardSidebar(),
+  dashboardBody(
+    fluidPage(
+      fluidRow(
+        aisModUI("ais")
+      ))
+  )
 )
+
 
 server <- function(input, output, session){
   callModule(aisMod, "ais")
