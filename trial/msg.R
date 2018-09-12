6f0b822f623f65595d9ba1346831c251824ca347
Yusman BK <ybkamaleri@gmail.com>
2018-09-12 14:22:53 +0200

    melding eller forklaring

---
+++ trial/msg.R
@@ -0,0 +1,5 @@
+bsTooltip(id = ns("til_lumb"),
+          title = "Telling for minst en av de allerede spesifiserte AIS koder",
+          placement = "right",
+          trigger = "hover",
+          options = list(container = "body")
