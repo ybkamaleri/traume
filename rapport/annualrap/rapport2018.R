9d931772fbd081490949893531f0f4f6d0cd9e8b
Yusman BK <ybkamaleri@gmail.com>
2018-09-17 22:26:28 +0200

    regionsvis antall traume kjønn og alder

--- rapport/annualrap/rapport2018.R
+++ rapport/annualrap/rapport2018.R
@@ -2,7 +2,7 @@
 
 rm(list = ls())
 
-pkg <- c("data.table", "rreg", "ggplot2")
+pkg <- c("data.table", "rreg", "ggplot2", "directlabels")
 
 sapply(pkg, require, character.only = TRUE)
 
@@ -72,17 +72,47 @@ bNA(ageMK)
 ageMK[, alle := mann + kvinne, by = list(RHF, age)]
 
 ## Gir nytt navn
-newNavn <- c("RHF", "Alder", "Menn", "Kvinner", "Alle")
+newNavn <- c("RHF", "Alder", "Menn", "Kvinner", "Begge")
 data.table::setnames(ageMK, 1:5, newNavn)
 ageMK
 
 ## konverterer data til long
 dataLong <-melt(ageMK, id.vars=c("RHF", "Alder"),
-                measure.vars=c("Menn","Kvinner","Alle"),
-                  variable.name="gender", value.name="n")
+                measure.vars=c("Menn","Kvinner","Begge"),
+                variable.name="gender", value.name="n")
+
+cols <- c("#4292c6", "#c6dbef", "#084594")
+
+pthemes <- theme(axis.text.y = element_text(size = 10, color = "black"),
+                 axis.text.x = element_text(size = 10, color = "black"),
+                 axis.ticks.y = element_blank(),
+                 axis.line.x = element_line(size = 0.5),
+                 axis.title.y = element_text(size = 11),
+                 axis.title.x = element_text(size = 11),
+                 panel.background = element_rect(fill = "white"),
+                 panel.border = element_rect(linetype = 1, fill = NA, color = "white"),
+                 panel.grid.minor.x = element_blank(),
+                 panel.grid.major.y = element_line(linetype = 2, color = "grey"),
+                 legend.position = "bottom",
+                 legend.title = element_blank(),
+                 legend.text = element_text(size = 11),
+                 legend.key = element_rect(fill = "white")
+                 )
 
 (antallTraume <- ggplot(dataLong) +
-   geom_line(aes(x = Alder, y = n, group = gender, color = gender)) + facet_wrap( ~ RHF))
+   geom_line(aes(x = Alder, y = n, group = gender, color = gender), size = 0.7) +
+   ## scale_linetype_manual(values = c(1,1,4)) +
+   scale_color_manual(values = cols) +
+   pthemes +
+   #kontrol for box for RHF navn
+   theme(strip.background = element_rect(colour = "white", fill = "white"),
+         strip.text.x = element_text(colour = "black", face = "bold", size =14)) +
+   scale_y_continuous(expand = c(0, 0)) +
+   scale_x_continuous(breaks = seq(0,110,10)) +
+   geom_hline(yintercept = 0, size = 1, color = "black", linetype = "solid") +
+   labs(x = "Alder", y = "Antall traume") +
+   facet_wrap( ~ RHF))
+
 
 
 ## 2017 vs 2016
